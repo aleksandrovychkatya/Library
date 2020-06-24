b2c9456077572d8ce4fd797a7d36729b05304dfb
DESKTOP-SN1CSC4\Chamomilla <aleksandrovychkatya@gmail.com>
2020-06-24 17:51:31 +0300

    Добавьте файлы проекта.

---
+++ Library/Models/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class Book : BaseEntity
+    {
+
+        [Display(Name = "Название")]
+        public string Title { get; set; }
+
+        [Display(Name = "Автор")]
+        public string Author { get; set; }
+
+        [Display(Name = "Жанр")]
+        public string Genre { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Издательство")]
+        public string Publisher { get; set; }
+
+        [Display(Name = "Рейтинг")]
+        [DisplayFormat(DataFormatString = "{0}/10")]
+        public int Rating { get; set; }
+    }
+}
